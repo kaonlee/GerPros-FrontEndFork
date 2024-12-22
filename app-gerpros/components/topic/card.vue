80be11fb4105803d06ec7bd720756e17dca8f7fa
Jiaen Lee <nn750137@gmail.com>
2024-12-22 09:58:24 +0800

    feat: topic layout

--- app-gerpros/components/topic/card.vue
+++ app-gerpros/components/topic/card.vue
@@ -1,21 +1,36 @@
 <template>
-  <!-- TODO: Min width -->
-  <div class="card lg:card-side lg:w-[80vw] bg-base-300 shadow-xl mt-4">
+  <div class="card lg:card-side lg:w-[100%] bg-base-300 shadow-xl mt-4">
     <figure>
       <img
-        :src="props.image.src"
-        :alt="props.image.alt"
+        class="lg:w-[200px] lg:h-[200px] h-[300px] w-[100%] object-cover object-center"
+        :src="props.topic.coverImage"
+        :alt="props.topic.title"
       >
     </figure>
     <div class="card-body">
       <h2 class="card-title">
-        {{ props.title }}
+        {{ props.topic.title }}
       </h2>
-      <p>{{ props.summary }}</p>
-      <div class="card-actions justify-end">
-        <button class="btn btn-primary">
-          閱讀更多
-        </button>
+      <p>{{ props.topic.abstract }}</p>
+      <div class="card-actions justify-between items-center">
+        <div class="tags flex flex-wrap gap-2">
+          <div
+            v-for="tag in props.topic.tags"
+            :key="tag"
+            class="badge badge-outline"
+          >
+            {{ tag }}
+          </div>
+        </div>
+        <NuxtLink
+          :to="{ name: 'topic-id', params: { id: props.topic?.id } }"
+        >
+          <button
+            class="btn btn-primary"
+          >
+            閱讀更多
+          </button>
+        </NuxtLink>
       </div>
     </div>
   </div>
@@ -23,20 +38,17 @@
 
 <script setup>
 const props = defineProps({
-  image: {
+  topic: {
     type: Object,
     default: () => ({
-      src: 'https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.webp',
-      alt: 'test',
+      title: 'title',
+      abstract: 'abstract',
+      coverImage: {
+        src: 'https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.webp',
+        alt: 'test',
+      },
+      tags: ['tag1', 'tag2'],
     }),
-  },
-  title: {
-    type: String,
-    default: 'title',
-  },
-  summary: {
-    type: String,
-    default: 'summary',
   },
 });
 </script>
--- app-gerpros/pages/topic/index.vue
+++ app-gerpros/pages/topic/index.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="article-wrapper">
+  <div class="topic-wrapper">
     <div class="cover-wrapper w-full h-96 bg-red-300">
-      <div class="contact-us">
+      <div class="header">
         <h2 class="zh">
           主題專欄
         </h2>
@@ -11,29 +11,46 @@
       </div>
     </div>
 
-    <TopicCard />
-
-    <div class="join mt-4">
-      <button class="join-item btn btn-md">
-        1
-      </button>
-      <button class="join-item btn btn-md btn-active">
-        2
-      </button>
-      <button class="join-item btn btn-md">
-        3
-      </button>
-      <button class="join-item btn btn-md">
-        4
-      </button>
+    <div class="flex gap-4 container mb-8">
+      <div class="grow">
+        <TopicCard
+          v-for="topic in topicList"
+          :key="topic.id"
+          :topic="topic"
+        />
+      </div>
+      <div class="flex-none w-[20%] h-100 p-4 border-l-2 border-gray-200">
+        <h2 class=" text-lg font-bold pb-2 mb-4 border-b-2 border-base-200">
+          主題標籤
+        </h2>
+        <div class="tags flex flex-wrap gap-2">
+          <div
+            v-for="tag in tagList"
+            :key="tag"
+            class="badge badge-outline"
+          >
+            {{ tag }}
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { TEST_TOPIC_LIST } from '~/constants';
+
+const topicList = ref({});
+const tagList = ref([]);
+
+onMounted(() => {
+  topicList.value = TEST_TOPIC_LIST;
+  tagList.value = Array.from(new Set(TEST_TOPIC_LIST.map(topic => topic.tags).flat()));
+});
+</script>
 
 <style scoped>
-.article-wrapper {
+.topic-wrapper {
   display: flex;
   position: relative;
   justify-content: center;
@@ -50,14 +67,14 @@
   background-repeat: no-repeat;
   background-position: center bottom;
 }
-.contact-us {
+.header {
   width: 90dvw;
   font-size: 2rem;
   color: white;
   text-shadow: 2px 2px 2px rgba(0, 0, 0, 0.2);
   margin-bottom: 3rem;
 }
-.contact-us .eng {
+.header .eng {
   font-size: 1.5rem;
 }
 </style>
